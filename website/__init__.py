73b4c7fdedd5f3b28ff5522c571e601a76d16fc4
Neal L <rlawton.cs@gmail.com>
2024-03-31 23:58:13 -0700

    Ininitialized database and created Note and User models.

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,19 @@
 ## This file makes the folder 'website' a python package. ##
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize our database object.
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "ourkey"
+    # Tell flask what database we're using and where it's located.
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # Initialize the database with the app.
+    db.init_app(app)
     # Import the blueprints.
     from .views import views
     from .auth import auth
--- website/models.py
+++ website/models.py
@@ -1,1 +1,27 @@
 ## This file contains our database models. ##
+
+# . means "From this package," import db.
+from . import db
+# A convenience class provided to simplify the implementation of 
+# user models in Flask applications that use Flask-Login for user 
+# authentication and session management. It provides default 
+# implementations for common user-related methods and properties, 
+# making it easier to work with user objects in Flask applications.
+# For example, properties like is_authenticated and methods like
+# get_id().
+from flask_login import UserMixin
+# func will help us store the time.
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    # SQLAlchemy will automatically add the current time using func.now()
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    
